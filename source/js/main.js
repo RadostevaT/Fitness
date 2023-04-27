bcaca41b6d82b3ea54b8e8722857b042eaa0c729
RadostevaT <radostevataisia@gmail.com>
2023-04-27 23:15:20 +0500

    header added

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initTabs} from './modules/tabs/init-tabs';
 
 // ---------------------------------
 
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    initTabs();
     const form = new Form();
     window.form = form;
     form.init();
